ed78164a525f6695be4176d23c41cab43661c15c
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-01-29 10:07:14 +0200

    new update

--- Fundamentals/week 2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-int firstNumber = int.Parse(Console.ReadLine());
-int secondNumber = int.Parse(Console.ReadLine());
-int thirdNumber = int.Parse(Console.ReadLine());
-int fourthNumber = int.Parse(Console.ReadLine());
-
-long result = ((firstNumber + secondNumber) / thirdNumber) * fourthNumber;
-
-Console.WriteLine(result);
