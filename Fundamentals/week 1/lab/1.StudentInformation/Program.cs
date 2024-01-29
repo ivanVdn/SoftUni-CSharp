6a523943bad2d490f7530a4c1068efe8816e158c
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-01-29 09:39:55 +0200

    update

---
+++ Fundamentals/week 1/lab/1.StudentInformation/Program.cs
@@ -0,0 +1,5 @@
+﻿var name = Console.ReadLine();
+var age = int.Parse(Console.ReadLine());
+var grade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
---
+++ Fundamentals/week 1/lab/10.MultiplicationTable/Program.cs
@@ -0,0 +1,8 @@
+﻿
+var input = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
+{
+    int product = input * i;
+    Console.WriteLine($"{input} X {i} = {product}");
+}
