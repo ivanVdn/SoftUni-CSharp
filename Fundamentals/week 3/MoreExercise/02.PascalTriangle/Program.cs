da24e0c80d5c8a20835338dcf7be9dff6d9b3167
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-01-29 13:33:43 +0200

    update

---
+++ Fundamentals/week 3/MoreExercise/02.PascalTriangle/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int numberOfRow = int.Parse(Console.ReadLine());
+int[] firstRow = { 1 };
+
+for (int i = 0; i < numberOfRow; i++)
+{
+    for (int j = 0; j < i + 1; j++)
+    {
+        Console.Write(firstRow[0] + " ");
+    }
+    Console.WriteLine();
+}
