84bbfda1ed0509eab23576b19cacfe30d55144bf
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-01-29 10:03:36 +0200

    updare 1

---
+++ Fundamentals/week 2/lab/03.ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int count = int.Parse(Console.ReadLine());
+decimal sum = 0;
+
+for (int i = 0; i < count; i++)
+{
+    decimal number = decimal.Parse(Console.ReadLine());
+    sum += number;
+}
+Console.WriteLine(sum);
---
+++ Fundamentals/week 2/lab/07.ConcatNames/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+string first = Console.ReadLine();
+string second = Console.ReadLine();
+string third = Console.ReadLine();
+
+Console.WriteLine($"{first}{third}{second}");
