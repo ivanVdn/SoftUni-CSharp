416de1f1878eec6f7ad95c5a9e0e5d2ede576f3d
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-02-07 10:39:58 +0200

    update

---
+++ Fundamentals/week 4/MoreExercise/03.LongerLine/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace _03.LongerLine
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            /*
+            // test 1
+                2
+                4
+                -1
+                2
+                -5
+                -5
+                4
+                -3
+
+            // test 2
+                34
+                -3
+                5
+                9
+                -8
+                10
+                8
+                11
+
+            */
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            PrintLongerLine(x1, y1, x2, y2, x3, y3, x4, y4);
+        }
+
+        static void PrintLongerLine(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            double x1y1 = DistanceToCenter(x1, y1);
+            double x2y2 = DistanceToCenter(x2, y2);
+            double x3y3 = DistanceToCenter(x3, y3);
+            double x4y4 = DistanceToCenter(x4, y4);
+
+
+
+            double distanceLine1 = x1y1 + x2y2;
+            double distanceLine2 = x3y3 + x4y4;
+
+            if (distanceLine1 >= distanceLine2)
+            {
+                if (x1y1 <= x2y2)
+                {
+                    Console.WriteLine($"({x1}, {y1})({x2}, {y2})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x2}, {y2})({x1}, {y1})");
+                }
+            }
+            else
+            {
+                if (x3y3 <= x4y4)
+                {
+                    Console.WriteLine($"({x3}, {y3})({x4}, {y4})");
+                }
+                else
+                {
+                    Console.WriteLine($"({x4}, {y4})({x3}, {y3})");
+                }
+            }
+        }
+
+        static double DistanceToCenter(double x, double y)
+        {
+            return Math.Abs(x + y); // => Math.Sqrt(x * x + y * y);
+        }
+    }
+
+}
---
+++ Fundamentals/week 4/MoreExercise/05.MultiplicationSign/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Threading.Channels;
+using Microsoft.VisualBasic;
+
+namespace _05.MultiplicationSign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            WriteResult(num1, num2, num3);
+        }
+
+        static int IsItZero(int num1, int num2, int num3)
+        {
+            int zero = 0;
+            if (num1 == 0)
+            {
+                zero++;
+            }
+
+            if (num2 == 0)
+            {
+                zero++;
+            }
+
+            if (num3 == 0)
+            {
+                zero++;
+            }
+
+            return zero;
+        }
+
+        static int IsItNegative(int num1, int num2, int num3)
+        {
+            int negative = 0;
+            if (num1 < 0)
+            {
+                negative++;
+            }
+
+            if (num2 < 0)
+            {
+                negative++;
+            }
+
+            if (num3 < 0)
+            {
+                negative++;
+            }
+
+            return negative;
+        }
+        
+        static void WriteResult(int num1, int num2, int num3)
+        {
+            if (IsItZero(num1, num2, num3) != 0)
+            {
+                Console.WriteLine("zero");
+            }
+            else if (IsItNegative(num1, num2, num3) % 2 == 0)
+            {
+                Console.WriteLine("positive");
+            }
+            else
+            {
+                Console.WriteLine("negative");
+            }
+        }
+    }
+}
