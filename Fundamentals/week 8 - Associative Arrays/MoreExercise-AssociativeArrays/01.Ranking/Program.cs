23acae3e9541e4d986cae964dcbe6cb7a61a7ca6
Иван Василев Иванов <ivan_vdn@hotmail.com>
2024-03-10 10:08:05 +0200

    UP

---
+++ Fundamentals/week 8 - Associative Arrays/MoreExercise-AssociativeArrays/01.Ranking/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _01.Ranking
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            var listOfContests = new Dictionary<string?, string?>();
+            var students = new Dictionary<string?, Dictionary<string?, int>>();
+
+            string? command;
+            while ((command = Console.ReadLine()) != "end of contests")
+            {
+                var cmd = command?
+                    .Split(":", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                var contestName = cmd?[0];
+                var password = cmd?[1];
+
+                listOfContests.Add(contestName, password);
+            }
+
+            while ((command = Console.ReadLine()) != "end of submissions")
+            {
+                var cmd = command?
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries)
+                    .ToList();
+                var contestName = cmd?[0];
+                var password = cmd?[1];
+                var username = cmd?[2];
+                var points = int.Parse(cmd?[3]);
+
+                if (!listOfContests.ContainsKey(contestName) || listOfContests[contestName] != password) continue;
+                if (!students.ContainsKey(username))
+                {
+                    students[username] = new Dictionary<string?, int>();
+                }
+
+                if (!students[username].ContainsKey(contestName))
+                {
+                    students[username][contestName] = 0;
+                }
+
+                if (points > students[username][contestName])
+                {
+                    students[username][contestName] = points;
+                }
+            }
+
+            var totalPoints = new Dictionary<string?, int>();
+            foreach (var user in students)
+            {
+                totalPoints[user.Key] = user.Value.Values.Sum();
+            }
+
+            var bestCandidate = totalPoints.MaxBy(x => x.Value).Key;
+            var bestCandidatePoints = totalPoints.MaxBy(x => x.Value).Value;
+
+            Console.WriteLine($"Best candidate is {bestCandidate} with total {bestCandidatePoints} points.");
+            Console.WriteLine("Ranking:");
+            foreach (var user in students.OrderBy(x => x.Key))
+            {
+                Console.WriteLine(user.Key);
+                foreach (var (key, value) in user.Value.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"#  {key} -> {value}");
+                }
+            }
+
+        }
+    }
+}
---
+++ Fundamentals/week 8 - Associative Arrays/MoreExercise-AssociativeArrays/02.Judge/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Drawing;
+
+namespace _02.Judge
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+
+            var contests = new Dictionary<string, Dictionary<string, int>>();
+            var userPoints = new Dictionary<string, int>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "no more time")
+            {
+                var tokens = input.Split(" -> ");
+
+                var username = tokens[0];
+                var contest = tokens[1];
+                var points = int.Parse(tokens[2]);
+
+                if (!contests.ContainsKey(contest))
+                {
+                    contests[contest] = new Dictionary<string, int>();
+                }
+
+                if (!contests[contest].ContainsKey(username))
+                {
+                    contests[contest][username] = points;
+                }
+                else
+                {
+                    contests[contest][username] = Math.Max(contests[contest][username], points);
+                }
+
+                if (!userPoints.ContainsKey(username))
+                {
+                    userPoints[username] = points;
+                }
+                else
+                {
+                    if (userPoints[username] < points)
+                    {
+                        userPoints[username] = points;
+                    }
+                    else
+                    {
+                        userPoints[username] += points;
+                    }
+                }
+            }
+
+            foreach (var kvp in contests)
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value.Count} participants");
+
+                var position = 1;
+                foreach (var participant in kvp.Value.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+                {
+                    Console.WriteLine($"{position}. {participant.Key} <::> {participant.Value}");
+                    position++;
+                }
+            }
+
+            Console.WriteLine("Individual standings:");
+
+            var userPosition = 1;
+            foreach (var user in userPoints.OrderByDescending(x => x.Value).ThenBy(x => x.Key))
+            {
+                Console.WriteLine($"{userPosition}. {user.Key} -> {user.Value}");
+                userPosition++;
+            }
+        }
+    }
+}
+
+
